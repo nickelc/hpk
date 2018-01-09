5b649371ae9749c35c9d6abedece29d233783ce2
Constantin Nickel <constantin.nickel@gmail.com>
2018-01-09 16:06:09 +0100

    Test for FragmentedReader::seek

--- src/hpk.rs
+++ src/hpk.rs
@@ -627,8 +627,37 @@ mod tests {
     }
     // }}}
 
+    // trait PrintState {{{
+    trait PrintState {
+        fn print_state(&mut self);
+    }
+
+    impl<T: Read + Seek> PrintState for FragmentedReader<T> {
+
+        fn print_state(&mut self) {
+            println!("pos: {}", self.pos);
+            println!("inner pos: {:?}", self.inner.seek(SeekFrom::Current(0)));
+            print!("positions: ");
+            for pos in &self.fragments {
+                print!("{} ", pos.end_pos);
+            }
+            println!();
+            println!("fragment states:");
+            for (i, s) in self.fragments.iter().enumerate() {
+                println!(
+                    "{}: off: {} len: {} limit: {}",
+                    i,
+                    s.offset,
+                    s.length,
+                    s.limit
+                );
+            }
+        }
+    }
+    // }}}
+
     #[test]
-    fn test_fragmented_reader_read() {
+    fn fragmented_reader_read() {
         let sample = vec![
             (10, 12, 0x11),
             (32, 20, 0x22),
@@ -672,7 +701,7 @@ mod tests {
     }
 
     #[test]
-    fn test_fragmented_reader_read_exact() {
+    fn fragmented_reader_read_exact() {
         let sample = vec![
             (10, 12, 0x11),
             (32, 20, 0x22),
@@ -703,6 +732,47 @@ mod tests {
         assert_eq!(&buf[32..64], [0x33; 32]);
         assert_eq!(&buf[64..67], [0x33; 3]);
         assert_eq!(&buf[67..89], [0x44; 22]);
+    }
+
+    #[test]
+    fn fragmented_reader_seek() {
+        let sample = vec![
+            (10, 12, 0x11),
+            (32, 20, 0x22),
+            (60, 35, 0x33),
+            (100, 22, 0x44),
+        ];
+        let mut r = create_fragmented_reader!(128, 0xFF, sample);
+
+        assert_eq!(r.len(), 89);
+
+        let mut buf = [0; 2];
+        let ret = r.seek(SeekFrom::Start(11)).unwrap();
+        assert_eq!(ret, 11);
+        r.read_exact(&mut buf).unwrap();
+        assert_eq!(buf, [0x11, 0x22]);
+
+        let ret = r.seek(SeekFrom::Current(18)).unwrap();
+        assert_eq!(ret, 31);
+        r.read_exact(&mut buf).unwrap();
+        assert_eq!(buf, [0x22, 0x33]);
+
+        let ret = r.seek(SeekFrom::End(-23)).unwrap();
+        assert_eq!(ret, 66);
+        r.read_exact(&mut buf).unwrap();
+        assert_eq!(buf, [0x33, 0x44]);
+
+        let ret = r.seek(SeekFrom::End(0)).unwrap();
+        assert_eq!(ret, 89);
+
+        assert_eq!(r.read(&mut buf).unwrap(), 0);
+        let ret = r.seek(SeekFrom::Start(12)).unwrap();
+        assert_eq!(ret, 12);
+
+        let mut buf = [0; 20];
+        let n = r.read(&mut buf).unwrap();
+        assert_eq!(n, 20);
+        assert_eq!(buf, [0x22; 20]);
     }
 }
 // }}}
